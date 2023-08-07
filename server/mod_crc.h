aea3e340026a8a78f392556f808cad82df607f89
sisrsis <virussisrsis@gmail.com>
2023-08-08 00:20:12 +0330

    add server   and   client folder

---
+++ server/mod_crc.h
@@ -0,0 +1,5 @@
+#ifndef MOD_CRC_H_INCLUDED
+#define MOD_CRC_H_INCLUDED
+
+unsigned short CRC16 ( unsigned char *puchMsg, unsigned short usDataLen  ); /* The function returns the CRC as a unsigned short type */
+#endif // MOD_CRC_H_INCLUDED
