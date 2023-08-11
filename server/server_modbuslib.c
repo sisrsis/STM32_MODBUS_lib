18e714779c40d814ddd91b1c546322d32cffa67d
sisrsis <virussisrsis@gmail.com>
2023-08-11 09:33:05 +0330

    It has been decided that the client and the server are in the same file

--- server/server_modbuslib.c
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-#include <stdio.h>
-#include "mod_crc.h"
-#include "server_modbuslib.h"
-
-
--- server/server_modbuslib.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SERVER_MODBUSLIB_H__
-#define __SERVER_MODBUSLIB_H__
-#include <stdint.h>
-#include <stdio.h>
-
-
-#endif
